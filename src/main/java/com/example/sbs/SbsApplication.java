786d14887a0bd3a74c10bf812b38d5f9da90ec40
naooung <1030n@naver.com>
2025-04-29 08:34:47 +0900

    :recycle: Refactor: @EnableJpaAuditing 자동 시간 기록 활성화

--- src/main/java/com/example/sbs/SbsApplication.java
+++ src/main/java/com/example/sbs/SbsApplication.java
@@ -2,12 +2,14 @@ package com.example.sbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SbsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SbsApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SbsApplication.class, args);
+  }
 
 }
